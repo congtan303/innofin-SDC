db7b650705d872832be4e62ec4cc6e77bc2bc36d
Your Name <you@example.com>
2022-12-28 16:18:08 +0700

    up up

--- src/index.js
+++ src/index.js
@@ -5,16 +5,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 
-  <HashRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL || ''}>
     
         <App />
 
-  </HashRouter>
+  </BrowserRouter>
 
 );
 
